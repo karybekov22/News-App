df32eb77f0c5fab9795e8a77f06d153e142e5054
Nursultan Karybekov <n.karybekov22@gmail.com>
2020-10-01 13:43:02 +0600

    Initial commit

---
+++ app/src/main/java/com/e/newsapp/ApiInterface.java
@@ -0,0 +1,17 @@
+package com.e.newsapp;
+
+import com.e.newsapp.Model.Headlines;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+
+    @GET("top-headlines")
+    Call<Headlines> getHeadlines(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
+}
